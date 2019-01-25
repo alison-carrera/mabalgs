fd4bf9717bef11648675c792871d953625b78849
renanoliveira <renan.gpa@gmail.com>
2019-01-25 15:44:53 -0200

    UCBTuned is child of UCB

--- mab/algs.py
+++ mab/algs.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-class UCB1:
+class UCB1(object):
     def __init__(self, n_arms):
         self.number_of_selections = np.zeros(n_arms).astype(np.float)
         self.rewards = np.zeros(n_arms).astype(np.float)
@@ -34,34 +34,13 @@ class UCB1:
         self.rewards[chosen_arm] += 1
 
 
-class UCBTuned:
+class UCBTuned(UCB1):
     def __init__(self, n_arms):
-        self.number_of_selections = np.zeros(n_arms).astype(np.float)
-        self.rewards = np.zeros(n_arms).astype(np.float)
-
-    def select(self):
-        arm_dont_usage = np.where(self.number_of_selections == 0)[0]
-        if len(arm_dont_usage) > 0:
-            self.number_of_selections[arm_dont_usage[0]] += 1
-            return arm_dont_usage[0]
-
-        average_reward = self.rewards / self.number_of_selections
-        total_counts = np.sum(self.number_of_selections)
-
-        ucb_values = self._factor_importance_each_arm(
-            total_counts,
-            self.number_of_selections,
-            average_reward
-            )
-        chosen_arm = np.argmax(ucb_values)
-
-        self.number_of_selections[chosen_arm] += 1
-
-        return chosen_arm
+        super().__init__(n_arms)
 
     def _factor_importance_each_arm(self, counts, num_selections, avg_reward):
         variance = (np.sum(np.square(self.rewards - avg_reward)))
-        variance_factor = (1/self.num_selections) * variance
+        variance_factor = (1/num_selections) * variance
 
         tuned = np.sqrt(2 * np.log(counts) / num_selections)
         tuned_factor = variance_factor + tuned
@@ -70,9 +49,6 @@ class UCBTuned:
         exploration_factor = np.sqrt((np.log(counts) / num_selections) * explo)
 
         return avg_reward + exploration_factor
-
-    def reward(self, chosen_arm):
-        self.rewards[chosen_arm] += 1
 
 
 class ThompsomSampling:
