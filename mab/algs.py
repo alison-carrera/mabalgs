7dd0955dfc37aac983c1c044d6cae61f99fb3e2b
renanoliveira <renan.gpa@gmail.com>
2019-01-25 12:17:31 -0200

    pycodestyle

--- mab/algs.py
+++ mab/algs.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class UCB1:
     def __init__(self, n_arms):
         self.number_of_selections = np.zeros(n_arms).astype(np.float)
@@ -10,57 +11,69 @@ class UCB1:
         if len(arm_dont_usage) > 0:
             self.number_of_selections[arm_dont_usage[0]] += 1
             return arm_dont_usage[0]
-      
+
         average_reward = self.rewards / self.number_of_selections
         total_counts = np.sum(self.number_of_selections)
-       
-        ucb_values = self._factor_importance_each_arm(total_counts, self.number_of_selections, average_reward)
+
+        ucb_values = self._factor_importance_each_arm(
+            total_counts,
+            self.number_of_selections,
+            average_reward
+            )
         chosen_arm = np.argmax(ucb_values)
-        
+
         self.number_of_selections[chosen_arm] += 1
-        
+
         return chosen_arm
-        
-    def _factor_importance_each_arm(self, total_counts, number_of_selections, average_reward):
-        exploration_factor = np.sqrt(2 * np.log(total_counts) / number_of_selections)
-        return average_reward + exploration_factor
-        
+
+    def _factor_importance_each_arm(self, counts, num_selections, avg_reward):
+        exploration_factor = np.sqrt(2 * np.log(counts) / num_selections)
+        return avg_reward + exploration_factor
+
     def reward(self, chosen_arm):
         self.rewards[chosen_arm] += 1
 
+
 class UCBTuned:
     def __init__(self, n_arms):
-      self.number_of_selections = np.zeros(n_arms).astype(np.float)
-      self.rewards = np.zeros(n_arms).astype(np.float)
+        self.number_of_selections = np.zeros(n_arms).astype(np.float)
+        self.rewards = np.zeros(n_arms).astype(np.float)
 
     def select(self):
-      arm_dont_usage = np.where(self.number_of_selections == 0)[0]
-      if len(arm_dont_usage) > 0:
-        self.number_of_selections[arm_dont_usage[0]] += 1
-        return arm_dont_usage[0]
+        arm_dont_usage = np.where(self.number_of_selections == 0)[0]
+        if len(arm_dont_usage) > 0:
+            self.number_of_selections[arm_dont_usage[0]] += 1
+            return arm_dont_usage[0]
 
-      average_reward = self.rewards / self.number_of_selections
-      total_counts = np.sum(self.number_of_selections)
+        average_reward = self.rewards / self.number_of_selections
+        total_counts = np.sum(self.number_of_selections)
 
-      ucb_values = self._factor_importance_each_arm(total_counts, self.number_of_selections, average_reward)
-      chosen_arm = np.argmax(ucb_values)
+        ucb_values = self._factor_importance_each_arm(
+            total_counts,
+            self.number_of_selections,
+            average_reward
+            )
+        chosen_arm = np.argmax(ucb_values)
 
-      self.number_of_selections[chosen_arm] += 1
+        self.number_of_selections[chosen_arm] += 1
 
-      return chosen_arm
+        return chosen_arm
 
-    def _factor_importance_each_arm(self, total_counts, number_of_selections, average_reward):
-      
-      variance_factor = (1/self.number_of_selections) * (np.sum(np.square(self.rewards - average_reward)))
-        
-      tuned_factor = variance_factor + np.sqrt(2 * np.log(total_counts) / number_of_selections)      
-      
-      exploration_factor = np.sqrt((np.log(total_counts) / number_of_selections) * np.minimum(1/4, tuned_factor))
-      
-      return average_reward + exploration_factor
+    def _factor_importance_each_arm(self, counts, num_selections, avg_reward):
+        variance = (np.sum(np.square(self.rewards - avg_reward)))
+        variance_factor = (1/self.num_selections) * variance
+
+        tuned = np.sqrt(2 * np.log(counts) / num_selections)
+        tuned_factor = variance_factor + tuned
+
+        explo = np.minimum(1/4, tuned_factor)
+        exploration_factor = np.sqrt((np.log(counts) / num_selections) * explo)
+
+        return avg_reward + exploration_factor
 
     def reward(self, chosen_arm):
-      self.rewards[chosen_arm] += 1
+        self.rewards[chosen_arm] += 1
+
 
 class ThompsomSampling:
     def __init__(self, n_arms):
@@ -69,13 +82,14 @@ class ThompsomSampling:
         self.n_arms = n_arms
 
     def select(self):
-        theta_value = np.random.beta(self.number_reward_1 + 1, self.number_reward_0 + 1)
-        
+        theta = self.number_reward_1 + 1, self.number_reward_0 + 1
+        theta_value = np.random.beta(theta)
+
         chosen_arm = np.argmax(theta_value)
-        
+
         self.number_reward_0[chosen_arm] += 1
-        
+
         return chosen_arm
-  
+
     def reward(self, chosen_arm):
         self.number_reward_1[chosen_arm] += 1
