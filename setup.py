880861b292ad7c38eb57331378d18523d54716c0
Alison Carrera <alison.carrera@corp.globo.com>
2021-05-10 20:53:35 -0300

    Removed fixed numpy version from install.

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='mabalgs',
-      version='0.6.5',
+      version='0.6.6',
       description='Multi-armed bandit algorithms',
       url='https://github.com/alison-carrera/mabalgs',
       author='Alison Carrera',
       author_email='alison.carrera2007@gmail.com',
       packages=find_packages(),
-      install_requires=['numpy==1.20.2'],
+      install_requires=['numpy'],
       long_description=long_description,
       long_description_content_type='text/markdown',
       license='Apache 2.0',
